bb6f78f5d5117016a8682aee8a91830c90b12759
Recep Çiftçi <recepagah12@gmail.com>
2022-05-09 21:30:26 +0300

    Viewmodels Updated

--- Teleperformance_Shopping.API/ViewModels/ShoppingListProductViewModel.cs
+++ Teleperformance_Shopping.API/ViewModels/ShoppingListProductViewModel.cs
@@ -2,7 +2,9 @@
 {
     public class ShoppingListProductViewModel
     {
+        public int ListProductId { get; set; }
         public ProductViewModel Product { get; set; }
         public int Amount { get; set; }
+        public bool IsAddedToCart { get; set; }
     }
 }
---
+++ Teleperformance_Shopping.API/ViewModels/UserIdTokenViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Teleperformance_Shopping.API.ViewModels
+{
+    public class UserIdTokenViewModel
+    {
+        public int UserId { get; set; }
+        public string Token { get; set; }
+    }
+}
